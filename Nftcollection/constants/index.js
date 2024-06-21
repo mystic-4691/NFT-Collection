e2bd2d099d117277dc3ffea639cea32a34994890
mystic-4691 <160069185+mystic-4691@users.noreply.github.com>
2024-06-21 18:27:06 +0530

    Add files via upload

---
+++ Nftcollection/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xDCbB2d86b71e829872A28DA25b7422Ad6fE2bD95";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
